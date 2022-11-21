3a3abe893f9654f8b37292951e0b6ba4f3d48b91
Nathan Aronson <njaronson421@gmail.com>
2022-11-21 07:37:00 -0500

    Initial Commit

---
+++ New Wave Store/New_Wave_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_StoreApp.swift
+//  New Wave Store
+//
+//  Created by Nathan Aronson (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
